0ac25aa3e1891ae1f5771618abe331705812b01d
Sang leng <sangleng@MacBook-Pro.local>
2021-07-13 21:49:44 +0700

    Topbar, Sidebar, Feed

---
+++ src/components/post/Post.jsx
@@ -0,0 +1,12 @@
+export default function Post () {
+    return (
+        <div className="post">
+            <div className="postWrapper">
+                <div className="postTop"></div>
+                <div className="postCenter"></div>
+                <div className="postBottom"></div>
+            </div>
+        </div>
+    )
+
+};
